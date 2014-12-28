acfeb0dba9778a9c82d4d14e7ed9a40505e96d4b
Nick Sagona <nick@nolainteractive.com>
2014-12-28 11:51:19 -0600

    First commit

---
+++ Captcha/config/module.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Module Name: Captcha
+ * Author: Nick Sagona
+ * Description: This is the CAPTCHA module for Phire CMS 2
+ * Version: 1.0
+ */
+return [
+    'Captcha' => [
+        'prefix'     => 'Captcha\\',
+        'src'        => __DIR__ . '/../src',
+        'routes'     => [
+            '/captcha[/]' => [
+                'controller' => 'Captcha\Controller\IndexController',
+                'action'     => 'index',
+            ]
+        ],
+        'config'     => [
+            'expire'      => 300,
+            'length'      => 4,
+            'width'       => 71,
+            'height'      => 26,
+            'lineSpacing' => 5,
+            'lineColor'   => [175, 175, 175],
+            'textColor'   => [0, 0, 0],
+            'font'        => null,
+            'size'        => 0,
+            'rotate'      => 0
+        ]
+    ]
+];
